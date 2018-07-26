962bbd0d5479b061dc28eb3c71672037b5c02bc0
Tomas Isdal <tomas.isdal@gmail.com>
2018-07-26 16:16:21 -0700

    Fix key-kay typo

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 key-native-extension
 =============
 
-![](logo.png)
-
 Chrome extension that adds pronounciation help for words starting with kn (like Knative).
 
 (based on cloud-to-butt by panicsteve)
+
+![](kay-extension.png)
--- Source/content_script.js
+++ Source/content_script.js
@@ -34,7 +34,7 @@ function handleText(textNode)
 	var v = textNode.nodeValue;
 
 	v = v.replace(/\b[Kk]n[a-zA-Z]+\b/g, function (x) {
-        	return x + " (pronounced key-" + x.substring(1) + ")";
+        	return x + " (pronounced kay-" + x.substring(1) + ")";
         });
 
 	textNode.nodeValue = v;
